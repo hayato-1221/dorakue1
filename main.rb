7fa762808fd0a059203c34a566302ffce4d0a8ca
hayato-1221 <hiperformance888@gmail.com>
2021-01-15 21:00:53 +0900

    ゆうしゃと敵の攻撃１回ずつ

--- main.rb
+++ main.rb
@@ -8,18 +8,22 @@ class Character
   end
 end
   class Brave < Character
-    def attack
+    def attack(monster)
+      puts "#{monster.name}があらわれた"
       puts "#{name}の攻撃！"
-
+      puts "#{monster.name}に 163 のダメージを与えた"
     end
 
   end
   brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
 
   class Monster < Character
-    def attack
-
+    def attack(brave)
+      puts "#{name}の攻撃！"
+      puts "#{brave.name}は 76 のダメージを受けた"
     end
 
   end
   monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
+  puts brave.attack(monster)
+  puts monster.attack(brave)
