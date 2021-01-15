ec4184c9a6734a3dc691c3a0833e0c6798ef884b
hayato-1221 <hiperformance888@gmail.com>
2021-01-15 22:21:07 +0900

    ダメージ計算式のリファクタリング

--- main.rb
+++ main.rb
@@ -9,25 +9,24 @@ class Character
 end
   class Brave < Character
     def attack(monster)
+      damage = (offense - monster.defense) / 2
       puts "#{monster.name}があらわれた"
       puts "#{name}の攻撃！"
-      puts "#{monster.name}に #{(offense - monster.defense) / 2} のダメージを与えた"
+      puts "#{monster.name}に #{damage} のダメージを与えた"
+    end
+
+  end
+  
+
+  class Monster < Character
+    def attack(brave)
+      damage = (offense - brave.defense) / 2
+      puts "#{name}の攻撃！"
+      puts "#{brave.name}は #{damage} のダメージを受けた"
     end
 
   end
   brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
-
-  class Monster < Character
-    def attack(brave)
-      puts "#{name}の攻撃！"
-      puts "#{brave.name}は #{(offense - brave.defense) / 2} のダメージを受けた"
-    end
-
-    while (offense - brave.defense) / 2 do
-
-    end
-
-  end
   monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
-  puts brave.attack(monster)
-  puts monster.attack(brave)
+  brave.attack(monster)
+  monster.attack(brave)
