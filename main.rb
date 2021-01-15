57cfa4115601294825ee33cf21dcb84e36838bf0
hayato-1221 <hiperformance888@gmail.com>
2021-01-15 19:05:49 +0900

    init

--- main.rb
+++ main.rb
@@ -7,12 +7,19 @@ class Character
     @defense = defense
   end
 end
-  class Brave
+  class Brave < Character
+    def attack
+      puts "#{name}の攻撃！"
+
+    end
 
   end
   brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
 
-  class Monster
+  class Monster < Character
+    def attack
+
+    end
 
   end
   monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
