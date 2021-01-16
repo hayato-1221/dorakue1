cdad8b1fbac22b8a0825e31f7dedecc4544af40c
hayato-1221 <hiperformance888@gmail.com>
2021-01-16 15:27:34 +0900

    後置if

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ end
   class Brave < Character
     def attack(monster)
       damage = (offense - monster.defense) / 2
-      monster.hp = monster.hp - damage
+      monster.hp = monster.hp - damage if monster.hp  -= 0
       puts "#{name}の攻撃！"
       puts "#{monster.name}に #{damage} のダメージを与えた"
     end
