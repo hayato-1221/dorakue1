a67a2c48de0c6cf79f7054fddeb378522cf7737d
hayato-1221 <hiperformance888@gmail.com>
2021-01-18 17:30:30 +0900

    main内を修正

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require "./character.rb"
 require "./brave.rb"
 require "./monster.rb"
 
-brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
+brave = Brave.new(name: "ゆうしゃ", hp: 239, offense: 203, defense: 129)
 monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
 puts "#{monster.name}があらわれた"
 while monster.hp > 0 && brave.hp > 0 do
@@ -16,10 +16,8 @@ while monster.hp > 0 && brave.hp > 0 do
   TEXT
 end
 
-if monster.hp = 0
+if monster.hp == 0
   puts "#{monster.name}をやっつけた"
-elsif brave.hp = 0
+else 
   puts "#{brave.name}は死んでしまった"
-else
-  ""
 end
